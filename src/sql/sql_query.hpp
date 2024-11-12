b186962b27d7415bf580f2a19b7fffa4a2030efe
Aksenov Anton <a-akcenov@mail.ru>
2024-11-12 19:31:52 +0300

    Support kill switches in /admin/v1/configs endpoint

--- src/sql/sql_query.hpp
+++ src/sql/sql_query.hpp
@@ -9,9 +9,10 @@ FROM uservice_dynconf.configs
 
 inline constexpr std::string_view kInsertConfigValue = R"~(
 INSERT INTO uservice_dynconf.configs
-(service, config_name, config_value)
-SELECT $1, d.key, d.value::jsonb
-FROM jsonb_each_text($2) as d
+(service, config_name, config_value, config_mode)
+SELECT $1, d.key, d.value::jsonb, m.value::uservice_dynconf.mode
+FROM jsonb_each_text($2) as d, jsonb_each_text($3) as m
+WHERE d.key = m.key
 ON CONFLICT (service, config_name)
 DO UPDATE SET
 config_value = EXCLUDED.config_value,
