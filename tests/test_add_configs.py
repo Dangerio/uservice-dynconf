8c14a29aed1fe9fb1ffedb2d44a8720d4d914cbf
Aksenov Anton <a-akcenov@mail.ru>
2024-11-13 18:10:19 +0300

    Add error return tests for /admin/v1/configs endpoint

--- tests/test_add_configs.py
+++ tests/test_add_configs.py
@@ -95,7 +95,7 @@ async def test_redefinitions_configs(
         ({'configs': {}, 'service': 'my-service'}),
     ],
 )
-async def test_add_configs_400(
+async def test_missing_required_fields(
         service_client, request_data,
 ):
     response = await service_client.post(
@@ -105,4 +105,64 @@ async def test_add_configs_400(
     assert response.json() == {
         'code': '400',
         'message': 'Fields \'configs\' and \'service\' are required',
+    }
+
+
+@pytest.mark.parametrize(
+    'kill_switches_enabled, kill_switches_disabled',
+    [
+        (['FIRST_CONFIG'], ['FIRST_CONFIG']),
+        (['FIRST_CONFIG'], ['FIRST_CONFIG', 'SECOND_CONFIG']),
+        (['FIRST_CONFIG', 'SECOND_CONFIG'], ['FIRST_CONFIG'],),
+        (['FIRST_CONFIG', 'SECOND_CONFIG'], ['SECOND_CONFIG', 'THIRD_CONFIG']),
+    ],
+)
+async def test_overlapping_kill_switches(
+        service_client, kill_switches_enabled, kill_switches_disabled,
+):
+    response = await service_client.post(
+        '/admin/v1/configs', json={
+            'configs': {
+                config_id: 'value'
+                for config_id in kill_switches_enabled + kill_switches_disabled
+            },
+            'service': 'my-service',
+            'kill_switches_enabled': kill_switches_enabled,
+            'kill_switches_disabled': kill_switches_disabled,
+        },
+    )
+    assert response.status_code == 400
+    assert response.json() == {
+        'code': '400',
+        'message':
+            'Ids in \'kill_switches_enabled\' and \'kill_switches_disabled\' '
+            'must not overlap',
+    }
+
+
+@pytest.mark.parametrize(
+    'kill_switches_enabled, kill_switches_disabled',
+    [
+        (['ENABLED_KILL_SWITCH'], []),
+        ([], ['DISABLED_KILL_SWITCH']),
+        (['ENABLED_KILL_SWITCH'], ['DISABLED_KILL_SWITCH']),
+    ],
+)
+async def test_kill_switches_not_from_configs(
+        service_client, kill_switches_enabled, kill_switches_disabled,
+):
+    response = await service_client.post(
+        '/admin/v1/configs', json={
+            'configs': {'DYNAMIC_CONFIG': 'value'},
+            'service': 'my-service',
+            'kill_switches_enabled': kill_switches_enabled,
+            'kill_switches_disabled': kill_switches_disabled,
+        },
+    )
+    assert response.status_code == 400
+    assert response.json() == {
+        'code': '400',
+        'message':
+            'Fields \'kill_switches_enabled\' and \'kill_switches_disabled\' '
+            'must consist of ids from \'configs\' field',
     }
