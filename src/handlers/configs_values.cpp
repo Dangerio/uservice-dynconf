055de4b12d573c6c9edbadba83d67ead43f1f7aa
Aksenov Anton <a-akcenov@mail.ru>
2024-11-19 17:31:58 +0300

    Use codegen types in configs/values handler

--- src/handlers/configs_values.cpp
+++ src/handlers/configs_values.cpp
@@ -12,28 +12,6 @@
 
 namespace uservice_dynconf::handlers::configs_values::post {
 
-// namespace {
-// struct RequestData {
-//   std::vector<std::string> ids{};
-//   std::optional<std::chrono::time_point<std::chrono::system_clock>>
-//       update_since{};
-//   std::string service{};
-// };
-
-// RequestData ParseRequest(const userver::formats::json::Value &request) {
-//   RequestData result;
-//   result.ids = request["ids"].As<std::vector<std::string>>({});
-//   result.service = request["service"].As<std::string>({});
-//   if (auto str_time = request["updated_since"].As<std::string>({});
-//       !str_time.empty()) {
-//     result.update_since = {userver::utils::datetime::Stringtime(
-//         str_time, userver::utils::datetime::kDefaultTimezone,
-//         userver::utils::datetime::kRfc3339Format)};
-//   }
-//   return result;
-// }
-// } // namespace
-
 Handler::Handler(const userver::components::ComponentConfig &config,
                  const userver::components::ComponentContext &context)
     : HttpHandlerJsonBase(config, context),
@@ -45,24 +23,23 @@ userver::formats::json::Value Handler::HandleRequestJsonThrow(
     const userver::formats::json::Value &request_json,
     userver::server::request::RequestContext &) const {
   using TimePointTz = userver::utils::datetime::TimePointTz;
+
   const auto request_data = request_json.As<ConfigsValuesRequestBody>();
   const auto data = cache_.Get();
 
-  constexpr TimePointTz kMinTime(
-      std::chrono::system_clock::time_point::min());
+  constexpr TimePointTz kMinTime(std::chrono::system_clock::time_point::min());
   TimePointTz updated_at(kMinTime);
 
   const auto configs =
-      request_data.ids.has_value()
+      request_data.ids.has_value() && !request_data.ids.value().empty()
           ? data->FindConfigs(request_data.service, request_data.ids.value())
           : data->FindConfigsByService(request_data.service);
 
   std::vector<std::string> kill_switches_enabled;
   std::vector<std::string> kill_switches_disabled;
-  userver::formats::json::ValueBuilder configs_found;
-  LOG_DEBUG() << "Before for";
+  userver::formats::json::ValueBuilder configs_found{
+      userver::formats::json::MakeObject()};
   for (const auto &config : configs) {
-    LOG_DEBUG() << "Config in for: " << config->key.config_name << " " << config->config_value;
     if (config && request_data.update_since.value_or(kMinTime).GetTimePoint() <=
                       config->updated_at.GetUnderlying()) {
       configs_found[config->key.config_name] = config->config_value;
@@ -80,27 +57,13 @@ userver::formats::json::Value Handler::HandleRequestJsonThrow(
     }
   }
 
-  // userver::formats::json::ValueBuilder builder;
-  // builder["configs"] = result.ExtractValue();
-  // if (!kill_switches_enabled.IsEmpty()) {
-  //   builder["kill_switches_enabled"] = kill_switches_enabled.ExtractValue();
-  // }
-  // if (!kill_switches_disabled.IsEmpty()) {
-  //   builder["kill_switches_disabled"] = kill_switches_disabled.ExtractValue();
-  // }
-  // builder["updated_at"] =
-  //     updated_at == kMinTime ? userver::utils::datetime::Now() : updated_at;
-  // return builder.ExtractValue();
-  //     LOG_DEBUG() << "Config in if: " << config->key.config_name << " " << config->config_value;
-  //     builder[config->key.config_name] = config->config_value;
-  //     updated_at = std::max(updated_at, TimePointTz{config->updated_at});
-  //   }
-
   ConfigsValuesResponseBody response;
   response.configs.extra = configs_found.ExtractValue();
   response.kill_switches_enabled = std::move(kill_switches_enabled);
   response.kill_switches_disabled = std::move(kill_switches_disabled);
-  response.updated_at = updated_at == kMinTime  ? TimePointTz{userver::utils::datetime::Now()} : updated_at;
+  response.updated_at = updated_at == kMinTime
+                            ? TimePointTz{userver::utils::datetime::Now()}
+                            : updated_at;
   return userver::formats::json::ValueBuilder{response}.ExtractValue();
 }
 
