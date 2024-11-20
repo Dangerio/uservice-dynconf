3c43f0a0298566a4a1ec7bb4427c454a91cd7c07
Aksenov Anton <a-akcenov@mail.ru>
2024-11-20 12:53:23 +0300

    Use codegen type in constructing error for handlers

--- src/utils/make_error.cpp
+++ src/utils/make_error.cpp
@@ -1,5 +1,6 @@
 #include "make_error.hpp"
 
+#include "docs/api/api.hpp"
 #include "userver/formats/json/value.hpp"
 #include <string_view>
 #include <userver/formats/json/value_builder.hpp>
@@ -8,10 +9,8 @@ namespace uservice_dynconf::utils {
 
 userver::formats::json::Value MakeError(std::string_view code,
                                         std::string_view message) {
-  userver::formats::json::ValueBuilder builder;
-  builder["code"] = code;
-  builder["message"] = message;
-  return builder.ExtractValue();
+  handlers::Error result{code.data(), message.data()};
+  return userver::formats::json::ValueBuilder{result}.ExtractValue();
 }
 
 } // namespace uservice_dynconf::utils
